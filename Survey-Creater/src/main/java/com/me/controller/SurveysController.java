39c7da0996931bc5f29270ad96916f2705648d88
Dai Yu <135446082+Geshin1@users.noreply.github.com>
2023-06-13 09:00:55 +0000

    111

--- Survey-Creater/src/main/java/com/me/controller/SurveysController.java
+++ Survey-Creater/src/main/java/com/me/controller/SurveysController.java
@@ -32,19 +32,27 @@ public class SurveysController {
     @Autowired
     private RedisUtil redisUtil;
 
-    @GetMapping("/page")
-    @ResponseBody
-    public ReturnMessage returnPage(@RequestParam("pageId")String pageId){
+/**
+ * returnPage 方法用于处理 GET 请求 "/surveys/page"，返回指定 pageId 的问卷调查内容。
+ * @param pageId 要查询的问卷调查的 pageId。
+ * @return 如果查询结果为空，则返回一个 ReturnMessage 对象，否则将查询结果中的 content 字段作为字符串返回。
+ */
+@GetMapping("/page")
+@ResponseBody
+public ReturnMessage returnPage(@RequestParam("pageId")String pageId){
 
-        Surveys surveys = surveysService.selectOne(new EntityWrapper<Surveys>().eq("pageid",pageId));
+    // 使用 SurveysService 类的 selectOne 方法查询数据库中 pageid 字段等于 pageId 的记录
+    Surveys surveys = surveysService.selectOne(new EntityWrapper<Surveys>().eq("pageid",pageId));
 
-        if(surveys == null){
-            return new ReturnMessage(true);
-        }
-/*        String data =  StringEscapeUtils.escapeJava(surveys.getContent()) ;*/
-        String data = surveys.getContent();
-        return new ReturnMessage(true,data);
+    // 如果查询结果为空，则返回一个 ReturnMessage 对象
+    if(surveys == null){
+        return new ReturnMessage(true);
     }
+
+    // 否则将查询结果中的 content 字段作为字符串返回
+    String data = surveys.getContent();
+    return new ReturnMessage(true,data);
+}
 
     @PostMapping("/commit")
     @ResponseBody
--- Survey-Creater/src/main/java/com/me/security/Md5Utils.java
+++ Survey-Creater/src/main/java/com/me/security/Md5Utils.java
@@ -13,17 +13,40 @@ import java.security.NoSuchAlgorithmException;
  * @Data
  * @Version 1.0
  **/
+/**
+ * Md5Utils 类用于对密码进行加密，以保证密码的安全性。
+ */
 public class Md5Utils {
+    
+    /**
+     * EncoderByMd5 方法用于对字符串进行 MD5 加密。
+     * @param str 要加密的字符串。
+     * @return 经过 MD5 加密后的字符串。
+     * @throws NoSuchAlgorithmException 当指定的加密算法不存在时抛出此异常。
+     * @throws UnsupportedEncodingException 当指定的字符集不被支持时抛出此异常。
+     */
     private static String EncoderByMd5(String str) throws NoSuchAlgorithmException, UnsupportedEncodingException {
+        // 获取 MD5 实例
         MessageDigest md5 = MessageDigest.getInstance("MD5");
+        // 创建 BASE64Encoder 实例
         BASE64Encoder base64Encoder=new BASE64Encoder();
 
+        // 对字符串进行 MD5 加密，并将加密后的字节数组转换为字符串
         String newstr=base64Encoder.encode(md5.digest(str.getBytes("utf-8")));
         return newstr;
     }
 
+    /**
+     * EncodePassword 方法用于对密码进行多次 MD5 加密。
+     * @param password 要加密的密码。
+     * @return 经过多次 MD5 加密后的字符串。
+     * @throws UnsupportedEncodingException 当指定的字符集不被支持时抛出此异常。
+     * @throws NoSuchAlgorithmException 当指定的加密算法不存在时抛出此异常。
+     */
     public static String EncodePassword(String password) throws UnsupportedEncodingException, NoSuchAlgorithmException {
+        // 将密码和字符串 "xq" 拼接起来
         String str=password+"xq";
+        // 对拼接后的字符串进行三次 MD5 加密
         return EncoderByMd5(EncoderByMd5(EncoderByMd5(str)));
     }
 
